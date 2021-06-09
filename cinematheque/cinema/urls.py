c81607413ca705c867b790fdcd6a2555d2dee8f1
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-06-08 21:24:00 -0700

    Movie detail commit

--- cinematheque/cinema/urls.py
+++ cinematheque/cinema/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('movies/', views.movies, name='movies'),
+    path('moviegenres/', views.moviegenres, name='moviegenres'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('moviedetail/<int:id>', views.moviedetail, name='detail'),
 ]
+
--- cinematheque/cinema/views.py
+++ cinematheque/cinema/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import MovieGenre, Movie, Review
 
 # Create your views here.
@@ -8,4 +8,21 @@ def index(request):
 def movies(request):
     movie_list=Movie.objects.all()
     return render(request, 'cinema/movies.html', {'movie_list': movie_list})
+
+def moviedetail(request, id):
+    movie=get_object_or_404(Movie, pk=id)
+    return render(request, 'cinema/moviedetail.html', {'movie': movie})
     
+
+
+
+def moviegenres(request):
+    moviegenre_list=MovieGenre.objects.all()
+    return render(request, 'cinema/moviegenres.html', {'moviegenre_list': moviegenre_list})
+
+def reviews(request):
+    review_list=Review.objects.all()
+    return render(request, 'cinema/reviews.html', {'review_list': review_list})
+
+
+
