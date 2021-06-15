98fff26705c23d484620240daa5c48d3d338cd8b
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-06-15 14:54:20 -0700

    Message in the models commit

--- cinematheque/cinema/models.py
+++ cinematheque/cinema/models.py
@@ -17,7 +17,7 @@ class MovieGenre(models.Model):
         verbose_name_plural='moviegenres'
 
 class Movie(models.Model):
-    #Video is not working, couldn't figured out. Nees work more!
+    #Embed YouTube video is not working, couldn't figured out, need to work more!
     video=EmbedVideoField(blank=True)
     moviename=models.CharField(max_length=255)
     moviegenre=models.ForeignKey(MovieGenre, on_delete=models.DO_NOTHING)
